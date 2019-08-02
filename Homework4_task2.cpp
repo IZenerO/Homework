198f57450927a453f86b2d084dde34b12a55b458
IZenerO <romikgerus14@gmail.com>
2019-08-02 22:18:16 +0300

    Second task: created programm, where you can make all letters in lower register from your string to upper register. Ready for review.

---
+++ Homework4_task2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main () {
+  const uint32_t ARRAY_SIZE = 50;
+  std::cout << "Enter your string to convert it in upper register: ";
+  char user_str [ARRAY_SIZE];
+  std::cin >> user_str;
+
+  char *begin = user_str;
+  char *end = user_str + ARRAY_SIZE;
+  char count = 0;
+  std::cout << "Your string in upper register: ";
+  while (begin != end) {
+    if (*begin >= 'a' && *begin <= 'z') {
+      *begin += 'A' - 'a';
+      std::cout << *begin;
+    } else {
+      std::cout << *begin;
+    }
+    *(begin++) = ++count;
+  }
+}
