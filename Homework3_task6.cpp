ef765bdb15315529b90b8591e1140ca0a388d4f4
IZenerO <romikgerus14@gmail.com>
2019-08-03 16:03:10 +0300

    Sixth task: edited issue with magic number, fixed problem with incorrect types comparison, fixed initialization of variable, edited codestyle problem.

--- Homework3_task6.cpp
+++ Homework3_task6.cpp
@@ -5,22 +5,24 @@ int main () {
     std::cout << "How much numbers you want to enter? " << std::endl;
     uint32_t count_num;
     std::cin >> count_num;
-    if (count_num > 50) {
-      std::cout << "Please, enter less than 50 numbers!" << std::endl;
+    const uint32_t MAX_NUMBERS = 50;
+    if (count_num > MAX_NUMBERS) {
+      std::cout << "Please, enter less than " << MAX_NUMBERS << " numbers!" << std::endl;
       continue;
     }
     const int32_t LOW_RANGE_VALUE = -60;
     const int32_t HIGH_RANGE_VALUE = 90;
     std::cout << "Please, enter your numbers in range " << LOW_RANGE_VALUE << "..." << HIGH_RANGE_VALUE << std::endl;
-    int32_t num;
     int32_t sum = 0;
-    for (int i = 0; i < count_num; i++) {
+    size_t i = 0;
+    while (i != count_num) {
+      int32_t num;
       std::cin >> num;
+      ++i;
       if (num <= LOW_RANGE_VALUE || num >= HIGH_RANGE_VALUE) {
-      std::cout << "You entered number out of range!" << std::endl;
-      i--;
-      continue;
-    }
+        std::cout << "You entered number out of range!" << std::endl;
+        --i;
+      }
       std::cout << std::endl;
       if (num % 2 != 0) {
         sum += num;   
