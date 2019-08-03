97eee61691acd6b4840de5a45811a9f80a6a5bfc
IZenerO <romikgerus14@gmail.com>
2019-08-03 15:56:19 +0300

    Third task: edited cout, edited count, deleted useless endl.

--- Homework3_task3.cpp
+++ Homework3_task3.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
 int main () {
-  std::cout << "Enter number above 0: " << std::endl;
+  std::cout << "Enter number above 0 to calculate a number of setted bits: " << std::endl;
   uint32_t num;
   std::cin >> num;
   uint32_t count = 0;
   while (num != 0) {
-    if (num & 1) {
-      count++;
-    }
+    count += num & 1;
     num >>= 1;
   }
-  std::cout << std::endl;
-  std::cout << count << std::endl;
+  std::cout << "Number of setted bits is: " << count;
   return 0;
 }
