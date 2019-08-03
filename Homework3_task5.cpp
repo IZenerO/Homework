1e6fc23effcc7ab73c9e9112f8a80aa16924e420
IZenerO <romikgerus14@gmail.com>
2019-08-03 16:00:36 +0300

    Fifth task: edited cout, deleted useless while, rewrited programm, now it reverses number.

--- Homework3_task5.cpp
+++ Homework3_task5.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
 
 int main () {
-  std::cout << "Enter you number: ";
+  std::cout << "Enter you number to reverse it: ";
   int32_t user_number;
   std::cin >> user_number;
-  int32_t num = user_number;
-  int32_t remainder = 1;
   const int32_t DIVIDER = 10;
-  uint32_t count = 1;
-  
-  while ((user_number /= DIVIDER) != 0) {
-    count++;
+  int32_t reverse_value = 0;
+
+  while (user_number != 0) {
+    reverse_value = reverse_value * 10 + user_number % 10;
+    user_number /= DIVIDER;
   }
-  for (int i = 0; i < count; i++) {
-    int32_t temp_num = 0;
-    temp_num = (num / remainder ) % 10;  
-    int32_t reverse_number = temp_num;
-    remainder *= DIVIDER;
-    if (i >= 1) {
-      std::cout << abs (reverse_number);
-    } else {
-      std::cout <<  reverse_number;
-    }
-  }
+  std::cout << "Your reversed number: " << reverse_value;
 }
