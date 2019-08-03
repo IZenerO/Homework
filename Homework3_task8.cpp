44aedbdcc0f24cafcd6d3ba1cb62e5e4fa4cea0b
IZenerO <romikgerus14@gmail.com>
2019-08-03 16:06:06 +0300

    Eighth task: edited number of loops, fixed problem with comparison types, deleted useless continue, now program calculate dividers correclty

--- Homework3_task8.cpp
+++ Homework3_task8.cpp
@@ -4,28 +4,35 @@ int main () {
   std::cout << "Enter your number" << std::endl;
   uint32_t user_num;
   std::cin >> user_num;
-  const uint32_t NUM = user_num;
   const uint32_t DIVIDER = 10;
-  uint32_t count = 1;
-  uint32_t sum = 0;
-  uint32_t remainder = 1;
+  size_t i = 1;
   uint32_t max_divider = 0;
-  while ((user_num /= DIVIDER) > 0){
-    count++;
-  }
-  for (int i = 0; i < count; i++) {
-    uint32_t temp = 0;
-    temp = (NUM / remainder) % 10;
-    sum += temp;
-    remainder *= DIVIDER;
-  }
-  std::cout << "Max divider: ";
-  for (int i = 1; i < NUM; ++i) {
-    if (NUM % i == 0) {
+  uint32_t sum_divider; 
+
+  while (i < user_num) {
+    if (user_num % i == 0) {
       max_divider = i;
-    } else {
-      continue;
     }
+    if (i >= 10 && user_num % i == 0) {
+      sum_divider = 0;
+      uint32_t count_temp = i;
+      while (count_temp != 0) {
+        sum_divider += count_temp % 10;
+        count_temp /= DIVIDER; 
+      }
+    }
+    i++;
   }
-  std::cout << max_divider;
+
+  uint32_t sum = 0;
+  const uint32_t NUM = user_num;
+  while (user_num != 0) {
+    sum += user_num % 10;
+    user_num /= DIVIDER;
+  }
+  if (sum > sum_divider) {
+    std::cout << "Max divider: " << NUM;
+  } else {
+    std::cout << "Max divider: " << max_divider;
+  }
 }
