6059d9dbba242361b9390161d7f5a5030f38d8b8
IZenerO <romikgerus14@gmail.com>
2019-08-03 16:04:27 +0300

    Seventh task: edited cout, rewrited programm without loops, now it works correctly.

--- Homework3_task7.cpp
+++ Homework3_task7.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
 
 int main () {
-  std::cout << "Enter number above 0: " << std::endl;
+  std::cout << "Enter number above 0 to check it for setted bit: " << std::endl;
   uint32_t num;
   std::cin >> num;
-  std::cout << "Enter number to check bit: " << std::endl;
+  std::cout << "Enter number to check setted bit: " << std::endl;
   uint32_t check_num;
   std::cin >> check_num;
-  uint32_t count =0;
-  while (num != 0) {
-    if (num & 1) {
-      count++;
-    }
-    num >>= 1;
-    if (count == check_num) {
-      std::cout << "Yes";
-      return 0;
-    } else {
-      continue;
-    }
+  num >>= check_num - 1;
+  if (num & 1) {
+    std::cout << "Yes";
+  } else {
+    std::cout << "No";
   }
-  std::cout << "No";
   return 0;
 }
