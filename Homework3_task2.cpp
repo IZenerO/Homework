c8a02ec9dfb1808df2c669f30005c8244a30aaf5
IZenerO <romikgerus14@gmail.com>
2019-08-03 15:54:39 +0300

    Second task: edited cout, fixed loops: deleted loop for, edited loop while; reduced number of variables, fixed all warnings.

--- Homework3_task2.cpp
+++ Homework3_task2.cpp
@@ -2,30 +2,18 @@
 
 int main () {
 
-  std::cout << "Enter number above 0: ";
+  std::cout << "Enter number above 0 to calculate average of all digits: ";
   uint32_t user_num;
   std::cin >> user_num;
-  uint32_t count = 1;
-  const uint32_t NUM = user_num;
+  uint32_t count = 0;
   const uint32_t DIVIDER = 10;
-
-  while ((user_num /= DIVIDER) > 0){
+  uint32_t sum = 0;
+  while (user_num != 0) {
+    sum += user_num % 10;
+    user_num /= DIVIDER;
     count++;
   }
-  
-  if (NUM > 0) {
-    uint32_t sum = 0;
-    uint32_t remainder = 1;
-    for (int i = 0; i < count; i++) {
-      uint32_t temp = 0;
-      temp = (NUM / remainder) % 10;
-      sum += temp;
-      remainder *= DIVIDER;
-    }
-    double AVERAGE = static_cast<double>(sum) / count;
-    std::cout << AVERAGE;
-  } else {
-    std::cout << "You entered wrong number!";
-  }
+  const double AVERAGE = static_cast<double>(sum) / count;
+  std::cout << "Average of all digits is: " << AVERAGE;
   return 0;
 }
