b6acc5ebc2ad167ec53db715dfc98af5259f07d5
IZenerO <romikgerus14@gmail.com>
2019-08-03 16:25:38 +0300

    First task: initialized array, deleted useless variable 'count' edited moving logic of *begin

--- Homework4_task1.cpp
+++ Homework4_task1.cpp
@@ -3,17 +3,16 @@
 int main () {
   const uint32_t ARRAY_SIZE = 50;
   std::cout << "Enter your string to check it for numbers: ";
-  char user_str [ARRAY_SIZE];
+  char user_str [ARRAY_SIZE] = {};
   std::cin >> user_str;
 
   char *begin = user_str;
   char *end = user_str + ARRAY_SIZE;
-  char count = 0;
   std::cout << "Numbers in your string are: ";
-  while (begin != end) {
+  while (begin != end && *begin != '\0') {
     if (*begin >= '0' && *begin <= '9') {
       std::cout << *begin << " ";
     }
-    *(begin++) = ++count;
+    ++begin;
   }
 }
