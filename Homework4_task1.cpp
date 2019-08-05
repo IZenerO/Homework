e73a93e4cd1420484262dbc9700d452906b6eb7f
IZenerO <romikgerus14@gmail.com>
2019-08-05 18:53:53 +0300

    First task: deleted useless 'equal'

--- Homework4_task1.cpp
+++ Homework4_task1.cpp
@@ -3,7 +3,7 @@
 int main () {
   const uint32_t ARRAY_SIZE = 50;
   std::cout << "Enter your string to check it for numbers: ";
-  char user_str [ARRAY_SIZE] = {};
+  char user_str [ARRAY_SIZE] {};
   std::cin >> user_str;
 
   char *begin = user_str;
