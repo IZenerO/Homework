b119425c33bc0ca5a8551238485e454482d21b66
IZenerO <romikgerus14@gmail.com>
2019-08-25 14:21:05 +0300

    Rewrited whole programm, added another idea witj array operations to programm.

--- Homework7_task1.cpp
+++ Homework7_task1.cpp
@@ -1,25 +1,70 @@
 #include <iostream>
-
-struct Hash {
-  int key;
-  uint32_t num;
-  uint32_t count;
+#include <string>
+struct Array {
+  int add_element (int value);
+  int replace (uint32_t position_of_el, int new_value);
+  int swap (uint32_t first_pos, uint32_t second_pos);
+  void show () const;
+  constexpr static unsigned ARRAY_SIZE = 255;
+  int array[ARRAY_SIZE] {};
+  int position = 0;
 };
 
-int HashFunction(uint32_t k, uint32_t cnt) {
-  return (k%cnt);
+int Array::add_element (int value) {
+  if (position == ARRAY_SIZE) {
+    std::cout << "Array is full\n";
+    return 0;
+  }
+  array[position++] = value;
+  return array[position];
 }
 
-int main() {
-  Hash hs;
-  std::cout << "How much keys you want to enter?" << std::endl;
-  std::cin >> hs.num;
-  std::cout << "Maximum numbers of hash-codes? " << std::endl;
-  std::cin >> hs.count;
-  while (hs.num != 0) {
-    std::cout << "Your key: ";
-    std::cin >> hs.key;
-    std::cout << "HashFunction(" << hs.key <<")=" << HashFunction(hs.key, hs.count) << std::endl;
-    --hs.num;
+int Array::replace (uint32_t position_of_el, int new_value) {
+  if (position_of_el > ARRAY_SIZE) {
+    std::cout << "Out of range!\n";
+    return 0;
   }
+  array[position_of_el - 1] = new_value;
+  return array[position];
+}
+
+int Array::swap (uint32_t first_pos, uint32_t second_pos) {
+  if (first_pos > ARRAY_SIZE || second_pos > ARRAY_SIZE) {
+    std::cout << "Out of range!\n";
+    return 0;
+  }
+  uint32_t temp = array[first_pos - 1];
+  array[first_pos - 1] = array[second_pos - 1];
+  array[second_pos - 1] = temp; 
+  return array[position]; 
+}
+
+void Array::show () const {
+  for (uint32_t i = 0; i < ARRAY_SIZE; ++i) {
+    std::cout << array[i] << " ";
+  }
+}
+
+int main () {
+  Array arr;
+  std::cout << "How much elements you want to enter? ";
+  uint32_t num;
+  std::cin >> num;
+  while (num != 0) {
+    int value;
+    std::cin >> value;
+    arr.add_element(value);
+    --num;
+  }
+  std::cout << "Which elements you want to swap? ";
+  uint32_t position_of_first_element, position_of_second_element;
+  std::cin >> position_of_first_element >> position_of_second_element;
+  arr.swap(position_of_first_element, position_of_second_element);
+  
+  std::cout << "Enter the element you want to replace and new value: ";
+  uint32_t pos;
+  int value;
+  std::cin >> pos >> value;
+  arr.replace(pos, value);
+  arr.show();
 }
