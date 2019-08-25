4f4f85ea0a57ee0bc7d6f6044048bb8a7984e8b7
IZenerO <romikgerus14@gmail.com>
2019-08-25 14:36:39 +0300

    Header file for second task with class implementation and method declaration

---
+++ Homework8_task2_singleton.hpp
@@ -0,0 +1,16 @@
+class Singleton {
+public:
+  static Singleton& getInstance();
+  int add(int value);
+  void show () const;
+private:
+  Singleton();
+  Singleton(const Singleton& ms) = delete;
+  Singleton& operator=(const Singleton& ms) = delete;
+  ~Singleton();
+
+  static const unsigned ARRAY_SIZE = 255;
+  int array[ARRAY_SIZE] {};
+  int position = 0;
+
+};
