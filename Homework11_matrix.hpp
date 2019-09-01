74b13795f2f3fd9dcfc084cf167237c7340766a0
IZenerO <romikgerus14@gmail.com>
2019-09-01 15:41:06 +0300

    Header file for programm with class and methods declaration

---
+++ Homework11_matrix.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class Matrix {
+  public:
+    Matrix(int SizeOfMatrix);
+    Matrix(const Matrix &mc);
+    ~Matrix();
+    // void Fill (int max_value);
+    const int GetSize () const;
+    int & operator() (int row, int col);
+    const Matrix operator+ (const Matrix& val);
+    const Matrix operator- (const Matrix& val);
+    void Add_Element (int value);
+    void show ();
+  private:
+    int **Matrix_arr;
+    int Size;
+};
