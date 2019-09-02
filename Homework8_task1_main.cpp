41d5b7e2443983d51b4b5458a2e874b2809b77aa
IZenerO <romikgerus14@gmail.com>
2019-09-02 19:53:05 +0300

    .cpp with main function, added some code for interaction wit our class

--- Homework8_task1_main.cpp
+++ Homework8_task1_main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Homework8_task1_array.hpp"
 
+
 int main () {
   Array ar;
-  std::cout << "How much elements you want to enter? ";
+std::cout << "How much elements you want to enter? ";
   uint32_t num;
   std::cin >> num;
   while (num != 0) {
@@ -22,5 +23,11 @@ int main () {
   int value;
   std::cin >> pos >> value;
   ar.replace(pos, value);
+
+  std::cout << "Which element you want to get?";
+  std::cin >> pos;
+  ar.get_value(pos);
+  std::cout << std::endl;
+  std::cout << "Your array:\n";
   ar.show();
 }
