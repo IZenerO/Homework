39ef5bd0ff131a5e5542045184ca8250e9081789
IZenerO <romikgerus14@gmail.com>
2019-09-02 16:19:04 +0300

    header, commit for check

--- Homework11_matrix.hpp
+++ Homework11_matrix.hpp
@@ -5,11 +5,13 @@ class Matrix {
     Matrix(int SizeOfMatrix);
     Matrix(const Matrix &mc);
     ~Matrix();
-    // void Fill (int max_value);
+    void Fill (int max_value);
     const int GetSize () const;
     int & operator() (int row, int col);
     const Matrix operator+ (const Matrix& val);
     const Matrix operator- (const Matrix& val);
+    const Matrix operator* (Matrix &val);
+    const Matrix operator* (int scalar);
     void Add_Element (int value);
     void show ();
   private:
