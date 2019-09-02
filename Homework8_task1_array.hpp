0bcaf1ddebc0bdf7fef15766b4034c2a387b0adb
IZenerO <romikgerus14@gmail.com>
2019-09-02 19:52:04 +0300

    Header file of programm, added some methods.

--- Homework8_task1_array.hpp
+++ Homework8_task1_array.hpp
@@ -1,10 +1,14 @@
+//using comparator = std::function<bool(int a, int b)>;
+
 class Array {
  public:
   int add(int value);
   void show() const;
   int replace(uint32_t pos1, int value);
   int swap(uint32_t pos1, uint32_t pos2);
-  
+  bool bubble_sort ();
+  int get_value(uint32_t position);
+
  private:
   static const unsigned ARRAY_SIZE = 255;
   int array[ARRAY_SIZE] {};
