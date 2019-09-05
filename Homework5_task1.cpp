543a032895e3e1323ef2b9aa482b217b40b89a8d
IZenerO <romikgerus14@gmail.com>
2019-09-05 21:42:54 +0300

    Fixed errors with useless 'return', added new command 'help' and few messages for programm usability, fixed codestyle errors, deleted useless 'break' from 'default' case, added 'const' to all 'end' of buffers, to provide a barrier, that this pointer and his value won't change. Ready for review.

--- Homework5_task1.cpp
+++ Homework5_task1.cpp
@@ -8,6 +8,7 @@ enum EnumTest{
   MOVE,
   CUT,
   SHOW,
+  HELP,
   UNDEFINED
 };
 size_t arr_len (const char* temp) {
@@ -44,20 +45,21 @@ EnumTest check (const char *str ) {
       return CUT;
     } else if (strcmp(str,"show") == 0) {
       return SHOW;
+    } else if (strcmp(str,"help") == 0) {
+      return HELP;
     }
   }
   return UNDEFINED;
 }
-char* copy(char* dst, char* src, const char* end_of_src) {
+char* copy(char* dst, char* src, const char* const end_of_src) {
   if ((dst != nullptr && src != nullptr && end_of_src != nullptr)) {
       while (src != end_of_src) { 
         *dst++ = *src++;
       }
-    return dst;
   }
   return dst;
 }
-char* print (char *start, const char *end) {
+char* print (char *start, const char * const end) {
   if (start != nullptr && end != nullptr && start != end) {
     char arr[SIZE_FOR_ARRAYS] {};
     std::cin >> arr;
@@ -67,7 +69,6 @@ char* print (char *start, const char *end) {
       while (begin_arr != end_arr) {
         *start++ = *begin_arr++;
       }
-      return start;
     }
     if (*begin_arr != '\0' && *start != '\0') {
       char temp_print_arr [SIZE_FOR_ARRAYS] {};
@@ -78,12 +79,11 @@ char* print (char *start, const char *end) {
         *start++ = *begin_arr++;
       }
       copy(start, first_of_temp, last_of_temp);
-      return start;
     }
   }
   return start;
 }
-char *move (char *start, const char *end) {
+char *move (char *start, const char * const end) {
   if (start != nullptr && end != nullptr) {
     char way[SIZE_FOR_ARRAYS] {};
     uint32_t num;
@@ -96,7 +96,6 @@ char *move (char *start, const char *end) {
   }
   if (start >= end) {
     std::cout << "Out of range!" << std::endl;
-    return start;
   }
   return start; 
 }
@@ -107,11 +106,13 @@ char* cut (char *start, char *end) {
     while (start != new_end) {
       *start++ = *end++;
     }
-  return returned_end;
+    return returned_end;
   }
   return start;
 }
 int main () {
+  std::cout << "Hello, it's text editor, here you" <<
+  "can do different operations with text, for more info enter 'help'\n";
   char user_arr[SIZE_FOR_ARRAYS] {};
   char *begin = user_arr;
   char *end = user_arr + SIZE_FOR_ARRAYS;
@@ -120,66 +121,78 @@ int main () {
   uint32_t end_position;
   while (true) {
     std::cout << "Enter you command and text: ";
-    char command[10] {};
+    char command[SIZE_FOR_ARRAYS] {};
     std::cin >> command;
     size_t type = check(command);
     
     switch (type) {
-    case PRINT: {
-      start_position = 0;
-      end_position = 0;
-      begin = print(begin, end);
-    } 
-    break;
-    case SELECT: {
-      std::cin >> start_position >> end_position;
-      if (start_position >= SIZE_FOR_ARRAYS || end_position >= SIZE_FOR_ARRAYS) {
-        std::cout << "Your position is out of range!";
+      case PRINT: {
         start_position = 0;
         end_position = 0;
+        begin = print(begin, end);
+        break;
       }
-    }  
-    break;
-    case COPY: {
-      if (start_position != 0 && end_position != 0) {
-        copy(temp_arr, user_arr + start_position, user_arr + start_position + end_position);
-      } else {
-        std::cout << "You didn`t select word!" << std::endl;
+      case SELECT: {
+        std::cin >> start_position >> end_position;
+        if (start_position >= SIZE_FOR_ARRAYS || end_position >= SIZE_FOR_ARRAYS) {
+          std::cout << "Your position is out of range!";
+          start_position = 0;
+          end_position = 0;
+        }
+        break;
       }
-    } 
-    break;
-    case PASTE: {
-      if (arr_len(temp_arr) != 0) {
+      case COPY: {
+        if (start_position != 0 && end_position != 0) {
+          copy(temp_arr, user_arr + start_position, user_arr + start_position + end_position);
+        } else {
+          std::cout << "You didn`t select word!" << std::endl;
+        }
+        break;
+      }
+      case PASTE: {
+        if (arr_len(temp_arr) != 0) {
+          start_position = 0;
+          end_position = 0;
+          copy(begin, temp_arr, temp_arr + arr_len(temp_arr));
+          begin += arr_len(begin);
+        } else {
+          std::cout << "You didn`t copy any word, unable to paste." << std::endl;
+        }
+        break;
+      }
+      case MOVE: {
         start_position = 0;
         end_position = 0;
-        char *end_of_temp = temp_arr + arr_len(temp_arr);
-        copy(begin, temp_arr, end_of_temp);
-        begin += arr_len(begin);
-      } else {
-        std::cout << "You didn`t copy any word, unable to paste." << std::endl;
+        begin = move(begin, end);
+        break;
+      } 
+      case CUT: {
+        begin = user_arr + start_position;
+        end = begin + end_position;
+        begin = cut(begin, end);
+        break;
+      } 
+      case SHOW: {
+        std::cout << user_arr << std::endl;
+        break;
       }
-    } 
-    break;
-    case MOVE: {
-      start_position = 0;
-      end_position = 0;
-      begin = move(begin, end);
-    } 
-    break;
-    case CUT: {
-      begin = user_arr + start_position;
-      end = begin + end_position;
-      begin = cut(begin, end);
-    } 
-    break;
-    case SHOW: {
-      std::cout << user_arr << std::endl;
-    } 
-    break;
-    default:
-      std::cout << "You entered wrong command";
-      return 0;
-      break;
+      case HELP: {
+        std::cout << R"(There are 8 commands available including this command: 
+        print: 'some_text';
+        select: 'start end';
+        copy: copying selected text;
+        paste: pasting copied text; 
+        move: 'direction length';
+        cut: cutting selected text;
+        show: displays your buffer.
+        But remember, if you want to use copy or cut, you need to select some range of text!
+        Have a nice experience in using text editor!)" << std::endl;
+        break;
+      } 
+      default: {
+        std::cout << "You entered wrong command";
+        return 0;
+      }
     }
   }
 }
